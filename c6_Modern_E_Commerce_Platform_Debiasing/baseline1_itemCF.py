adac4539110db25248432dcac92b55a79a880d5d
ian <yongguiluo@hotmail.com>
2020-04-17 16:27:28 +0800

    sm

--- c6_Modern_E_Commerce_Platform_Debiasing/baseline1_itemCF.py
+++ c6_Modern_E_Commerce_Platform_Debiasing/baseline1_itemCF.py
@@ -1,4 +1,25 @@
 """
+日期:2020-04-17 16:07:36排名: 无
+
+score:0.0841
+hitrate_50_full:0.2068
+ndcg_50_full:0.0841
+hitrate_50_half:0.1581
+ndcg_50_half:0.0632
+改进：
+用所有的数据预测phasen
+初始版本是用phase0-n的数据预测phasen，而没有用到后面的数据
+
+日期:2020-04-17 15:22:28排名: 无
+修改了一行bug
+for j, wij in sorted(sim_item_corr[i].items(), reverse=True)[:top_k]:
+
+score:0.0787
+hitrate_50_full:0.1996
+ndcg_50_full:0.0787
+hitrate_50_half:0.1383
+ndcg_50_half:0.0556
+
 高手开源baseline
 https://tianchi.aliyun.com/forum/postDetail?postId=103530
 日期:2020-04-17 10:42:13
@@ -25,8 +46,8 @@ def get_sim_item(df, user_col, item_col, use_iif=False):
     user_item_ = df.groupby(user_col)[item_col].agg(set).reset_index()  
     user_item_dict = dict(zip(user_item_[user_col], user_item_[item_col]))  
   
-    sim_item = {}  
-    item_cnt = defaultdict(int)  
+    sim_item = {}  # sim_item[i][j] 表示item_i和item_j共现的次数
+    item_cnt = defaultdict(int)  # 统计某个item有多少个user购买过
     for user, items in tqdm(user_item_dict.items()):  
         for i in items:  
             item_cnt[i] += 1  
@@ -39,26 +60,36 @@ def get_sim_item(df, user_col, item_col, use_iif=False):
                     sim_item[i][relate_item] += 1  
                 else:  
                     sim_item[i][relate_item] += 1 / math.log(1 + len(items))  
+    
     sim_item_corr = sim_item.copy()  
     for i, related_items in tqdm(sim_item.items()):  
         for j, cij in related_items.items():  
-            sim_item_corr[i][j] = cij/math.sqrt(item_cnt[i]*item_cnt[j])  
-  
+            # item_i和item_j的共现系数 = 共现的次数/各自出现的次数之积
+            sim_item_corr[i][j] = cij/math.sqrt(item_cnt[i]*item_cnt[j]) 
     return sim_item_corr, user_item_dict  
-  
 
 def recommend(sim_item_corr, user_item_dict, user_id, top_k, item_num):  
+    """
+    向user_id推荐item_num个该user_id没有买过的item
+    """
     rank = {}  
+    # 该user_id购买过的items
     interacted_items = user_item_dict[user_id]  
+    # 遍历该user购买过的items，
     for i in interacted_items:  
-        for j, wij in sorted(sim_item_corr[i].items(), reverse=True)[0:top_k]:  
+        # 遍历该item共现最高的top_k个item，把其中用户没有买过的加入推荐列表
+#         for j, wij in sorted(sim_item_corr[i].items(), reverse=True)[:top_k]:  
+        for j, wij in sorted(sim_item_corr[i].items(), key=lambda d: d[1], reverse=True)[:top_k]:  
             if j not in interacted_items:  
                 rank.setdefault(j, 0)  
                 rank[j] += wij  
     return sorted(rank.items(), key=lambda d: d[1], reverse=True)[:item_num]  
   
-# fill user to 50 items  
 def get_predict(df, pred_col, top_fill):  
+    """
+    fill user to 50 items
+    逻辑就是如果推荐给用户的items少于50个，就用点击数最高的item补足
+    """
     top_fill = [int(t) for t in top_fill.split(',')]  
     scores = [-1 * i for i in range(1, len(top_fill) + 1)]  
     ids = list(df['user_id'].unique())  
@@ -74,32 +105,57 @@ def get_predict(df, pred_col, top_fill):
     df = df.groupby('user_id')['item_id'].apply(lambda x: ','.join([str(i) for i in x])).str.split(',', expand=True).reset_index()  
     return df  
 
+
 now_phase = 1  
 train_path = './data_origin/underexpose_train'  
 test_path = './data_origin/underexpose_test'  
 recom_item = []  
 
+# whole_click = pd.DataFrame()  
+# for c in range(now_phase + 1):  
+#     print('phase:', c)  
+#     click_train = pd.read_csv(train_path + '/underexpose_train_click-{}.csv'.format(c), header=None,  names=['user_id', 'item_id', 'time'])  
+#     click_test = pd.read_csv(test_path + '/underexpose_test_click-{}/underexpose_test_click-{}.csv'.format(c, c), header=None,  names=['user_id', 'item_id', 'time'])  
+
+#     all_click = click_train.append(click_test)  
+#     whole_click = whole_click.append(all_click)  
+#     item_sim_list, user_item = get_sim_item(whole_click, 'user_id', 'item_id', use_iif=True)  
+
+#     for i in tqdm(click_test['user_id'].unique()):  
+#         rank_item = recommend(item_sim_list, user_item, i, 500, 50)  
+#         for j in rank_item:  
+#             recom_item.append([i, j[0], j[1]])  
+# # find most popular items  
+# top50_click = whole_click['item_id'].value_counts().index[:50].values  
+# top50_click = ','.join([str(i) for i in top50_click])  
+
+# recom_df = pd.DataFrame(recom_item, columns=['user_id', 'item_id', 'sim'])  
+# result = get_predict(recom_df, 'sim', top50_click)  
+
 whole_click = pd.DataFrame()  
+click_test = pd.DataFrame()  
 for c in range(now_phase + 1):  
     print('phase:', c)  
     click_train = pd.read_csv(train_path + '/underexpose_train_click-{}.csv'.format(c), header=None,  names=['user_id', 'item_id', 'time'])  
-    click_test = pd.read_csv(test_path + '/underexpose_test_click-{}/underexpose_test_click-{}.csv'.format(c, c), header=None,  names=['user_id', 'item_id', 'time'])  
+    click_test1 = pd.read_csv(test_path + '/underexpose_test_click-{}/underexpose_test_click-{}.csv'.format(c, c), header=None,  names=['user_id', 'item_id', 'time'])  
 
-    all_click = click_train.append(click_test)  
+    all_click = click_train.append(click_test1)  
+    click_test = click_test.append(click_test1) 
     whole_click = whole_click.append(all_click)  
-    item_sim_list, user_item = get_sim_item(whole_click, 'user_id', 'item_id', use_iif=True)  
+    
+item_sim_list, user_item = get_sim_item(whole_click, 'user_id', 'item_id', use_iif=True)  
 
-    for i in tqdm(click_test['user_id'].unique()):  
-        rank_item = recommend(item_sim_list, user_item, i, 500, 50)  
-        for j in rank_item:  
-            recom_item.append([i, j[0], j[1]])  
+for i in tqdm(click_test['user_id'].unique()):  
+    rank_item = recommend(item_sim_list, user_item, i, 500, 50)  
+    for j in rank_item:  
+        recom_item.append([i, j[0], j[1]])  
 # find most popular items  
 top50_click = whole_click['item_id'].value_counts().index[:50].values  
 top50_click = ','.join([str(i) for i in top50_click])  
 
 recom_df = pd.DataFrame(recom_item, columns=['user_id', 'item_id', 'sim'])  
 result = get_predict(recom_df, 'sim', top50_click)  
-result.to_csv('/Users/luoyonggui/Downloads/baseline1_itemcf.csv', index=False, header=None)
+result.to_csv('/Users/luoyonggui/Downloads/baseline1_itemcf1.csv', index=False, header=None)
 
 t1 = time.time()
 print(f'{t1-t0}s')
